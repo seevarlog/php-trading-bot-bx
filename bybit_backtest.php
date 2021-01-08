3181d25e3eacd29e78e9f9db430645ef1124ca6a
seevarlog <sung135907@naver.com>
2021-01-08 20:34:50 +0900

    -추가

---
+++ bybit_backtest.php
@@ -0,0 +1,208 @@
+<?php
+
+
+include __DIR__."/vendor/autoload.php";
+
+use Lin\Bybit\BybitInverse;
+use Lin\Bybit\BybitLinear;
+use trading_engine\managers\CandleManager;
+use trading_engine\managers\OrderManager;
+use trading_engine\managers\PositionManager;
+use trading_engine\managers\TradeLogManager;
+use trading_engine\objects\Account;
+use trading_engine\objects\Candle;
+use trading_engine\objects\Order;
+use trading_engine\strategy\StrategyBB;
+use trading_engine\strategy\StrategyTest;
+use trading_engine\util\CoinPrice;
+use trading_engine\util\Config;
+use trading_engine\util\GlobalVar;
+use trading_engine\util\Notify;
+
+ini_set("display_errors", 1);
+ini_set('memory_limit','3G');
+
+$candle_mng = CandleManager::getInstance();
+$make_candle_min_list = [5, 15, 30, 60, 60*4, 60 * 24];
+for ($i=2; $i>0; $i--)
+{
+    foreach ($make_candle_min_list as $make_min)
+    {
+        $interval = $make_min;
+        if ($interval == 60 * 24)
+        {
+            $interval = "D";
+        }
+
+        // 일봉셋팅 (14일꺼 가져옴)
+        $candle_1day_list = $bybit->publics()->getKlineList([
+            'symbol'=>"BTCUSD",
+            'interval'=>$interval,
+            'from'=>time()-60*$make_min*180*$i
+        ]);
+        //var_dump($candle_1day_list);
+
+        // 1일봉 셋팅
+
+        $prev_candle = $candle_mng->getLastCandle($make_min);
+        foreach ($candle_1day_list['result'] as $candle_data)
+        {
+            $new_candle = new Candle($make_min);
+            $new_candle->t = $candle_data['open_time'];
+            $new_candle->o = $candle_data['open'];
+            $new_candle->h = $candle_data['high'];
+            $new_candle->l = $candle_data['low'];
+            $new_candle->c = $candle_data['close'];
+
+            if ($prev_candle != null)
+            {
+                $new_candle->cp = $prev_candle;
+                $prev_candle->cn = $new_candle;
+                $prev_candle = $new_candle;
+            }
+
+            CandleManager::getInstance()->addNewCandle($new_candle);
+        }
+        sleep(0.1);
+    }
+}
+
+// 계정 셋팅
+$account = Account::getInstance();
+$account->balance = 1;
+
+
+
+// 계정 밸런스 불러옴
+$account->balance = $bybit->privates()->getWalletBalance()["result"]["BTC"]["wallet_balance"];
+
+Notify::sendMsg("봇을 시작합니다. 시작 잔액 usd:".$account->getUSDBalance()." BTC:".$account->getBitBalance());
+
+try {
+    $candle_prev_1m = CandleManager::getInstance()->getLastCandle(1);
+    while (1) {
+        sleep(0.5);
+        if (time() % 900 == 0)
+        {
+            Notify::sendMsg("살아있음.");
+        }
+
+        $time_second = time() % 60;
+        // 55 ~ 05 초 사이에 갱신을 시도한다.
+        if (!($time_second < 10 || $time_second > 50)) {
+            continue;
+        }
+
+        // 캔들 마감 전에는 빨리 갱신한다.
+        $candle_api_result = $bybit->publics()->getKlineList([
+            'symbol' => "BTCUSD",
+            'interval' => "1",
+            'from' => time() - 60
+        ]);
+
+        if (!isset($candle_api_result['result'][0])) {
+            continue;
+        }
+
+        $candle_data = $candle_api_result['result'][0];
+        $candle_1m = new Candle(1);
+        $candle_1m->t = $candle_data['open_time'];
+        $candle_1m->o = $candle_data['open'];
+        $candle_1m->h = $candle_data['high'];
+        $candle_1m->l = $candle_data['low'];
+        $candle_1m->c = $candle_data['close'];
+        if ($candle_prev_1m->t == $candle_data['open_time'])
+        {
+            $candle_prev_1m->updateCandle($candle_data['high'], $candle_data['low'], $candle_data['close']);
+        }
+
+        if ($candle_prev_1m->t == $candle_1m->t) {
+            continue;
+        }
+
+
+        $candle_1m->cp = $candle_prev_1m;
+        $candle_prev_1m->cn = $candle_1m;
+        $candle_prev_1m = $candle_1m;
+        CandleManager::getInstance()->addNewCandle($candle_1m);
+
+        foreach ($make_candle_min_list as $min)
+        {
+            if ($candle_1m->t % (60 * $min) == 0)
+            {
+                $_last_candle = CandleManager::getInstance()->getLastCandle($min);
+                if ($_last_candle != null)
+                {
+                    $_last_candle->updateCandle($candle_1m->h, $candle_1m->l, $candle_1m->c);
+                }
+
+                $_new_last_candle = new Candle($min);
+                $_new_last_candle->setData($candle_1m->t, $candle_1m->o, $candle_1m->h, $candle_1m->l, $candle_1m->c);
+                CandleManager::getInstance()->addNewCandle($_new_last_candle);
+                $_new_last_candle->cp = $_last_candle;
+                if ($_last_candle != null)
+                {
+                    $_last_candle->cn = $_new_last_candle;
+                }
+                var_dump("캔들업데이트 : ".$min);
+                var_dump($_new_last_candle->displayCandle());
+            }
+            else
+            {
+                CandleManager::getInstance()->getLastCandle($min)->updateCandle($candle_1m->h, $candle_1m->l, $candle_1m->c);
+                var_dump("update : ".$min);
+                var_dump(CandleManager::getInstance()->getLastCandle($min)->displayCandle());
+            }
+        }
+
+        $order_count = count(OrderManager::getInstance()->getOrderList("BBS1"));
+        $position_msg = PositionManager::getInstance()->getPosition("BBS1")->getPositionMsg();
+        PositionManager::getInstance()->getPosition("BBS1");
+
+        foreach (OrderManager::getInstance()->getOrderList("BBS1") as $order) {
+            if ($order->is_stop == 1) {
+                $order_result_list = $bybit->privates()->getOrderList(
+                    [
+                        'symbol' => 'BTCUSD',
+                        'order_status' => 'Filled',
+                        'limit' => 3
+                    ]
+                );
+                foreach ($order_result_list['result']['data'] as $order_result) {
+                    if (!isset($order_result['last_exec_price']) || $order_result['last_exec_price'] == 0) {
+                        continue;
+                    }
+
+                    if ($order->order_id == $order_result['order_id']) {
+                        $order->execution_price = $order_result['last_exec_price'];
+                    }
+                }
+            }
+        }
+
+
+        CoinPrice::getInstance()->bit_price = $candle_1m->c;
+
+        // 오더북 체크크
+
+        OrderManager::getInstance()->update($candle_1m);
+        //$msg = StrategyTest::getInstance()->BBS($candle);
+        //$msg = StrategyBB::getInstance()->BBS($candle_1m);
+        //Notify::sendMsg("debug:".$msg);
+
+
+        if ($candle_1m->t % 1000)
+        {
+            $account = Account::getInstance();
+            $result = GlobalVar::getInstance()->
+            getByBit()->privates()->getWalletBalance()["result"]["BTC"]["wallet_balance"];
+            if ($result !== null)
+            {
+                $account->balance = $result;
+            }
+        }
+    }
+}catch (\Exception $e)
+{
+    var_dump($e);
+}
---
+++ getVolatility.php
@@ -0,0 +1,201 @@
+<?php
+
+use trading_engine\managers\CandleManager;
+use trading_engine\managers\TradeLogManager;
+use trading_engine\objects\Account;
+use trading_engine\objects\Candle;
+
+require_once('vendor/autoload.php');
+
+//error_reporting(E_ALL);
+
+ini_set("display_errors", 1);
+ini_set('memory_limit','4G');
+//ini_set("xdebug.overload_var_dump", "off");
+header('Content-Type: text/html; charset=UTF-8');
+
+ob_start();
+$time_start = time();
+if (!($fp = fopen(__DIR__.'/output.csv', 'r'))) {
+    echo "err";
+    return;
+}
+
+// m본
+$make_candle_min_list = [3, 5, 15, 30, 60, 60*4, 60 * 24];
+
+// 30분봉 만들어봄
+$candleMng = CandleManager::getInstance();
+$prev_candle = new Candle(1);
+$candle_list = array();
+$is_bybit_csv = false;
+$z = 0;
+for ($i=0; $i<35000000; $i++)
+{
+    if (feof($fp))
+    {
+        echo "!2";
+        break;
+    }
+
+    $arr = explode(",", fgets($fp,1024));
+
+    if ($i<=0)
+    {
+        if ($arr[1] == "symbol")
+        {
+            $is_bybit_csv = true;
+            $z = 2;
+        }
+        continue;
+    }
+
+    $candle = new Candle(1);
+    $candle->cp = $prev_candle;
+    $prev_candle->cn = $candle;
+    if ($arr[1] == "NaN")
+    {
+        $last_candle = $candleMng->getLastCandle(1);
+        $candle->setData($arr[0], $last_candle->o, $last_candle->h, $last_candle->l, $last_candle->c);
+    }
+    else
+    {
+        $candle->setData($arr[0], $arr[1 + $z], $arr[2 + $z], $arr[3 + $z], $arr[4 + $z]);
+    }
+
+    foreach ($make_candle_min_list as $min)
+    {
+        if ($candle->t % (60 * $min) == 0)
+        {
+            $_last_candle = $candleMng->getLastCandle($min);
+            if ($_last_candle != null)
+            {
+                $_last_candle->updateCandle($candle->h, $candle->l, $candle->c);
+            }
+
+            $_new_last_candle = new Candle($min);
+            $_new_last_candle->setData($candle->t, $candle->o, $candle->h, $candle->l, $candle->c);
+            $candleMng->addNewCandle($_new_last_candle);
+            $_new_last_candle->cp = $_last_candle;
+            if ($_last_candle != null)
+            {
+                $_last_candle->cn = $_new_last_candle;
+            }
+        }
+        else
+        {
+            $_candle = $candleMng->getLastCandle($min);
+            if ($_candle != null)
+            {
+                $_candle->updateCandle($candle->h, $candle->l, $candle->c);
+            }
+        }
+    }
+
+    /*
+    if ($candle->t % (3600*24) == 0)
+    {
+        $candle_new_1day->updateCandle($candle->h, $candle->l, $candle->c);
+        CandleManager::getInstance()->addNewCandle($candle_new_1day);
+
+        $candle_1day_prev = $candle_new_1day;
+        $candle_new_1day = new Candle(60 * 24);
+        $candle_new_1day->setData($candle->t, $candle->o, $candle->h, $candle->l, $candle->c);
+        $candle_new_1day->cp = $candle_1day_prev;
+        $candle_1day_prev->cn = $candle_new_1day;
+    }
+    else
+    {
+        $candle_new_1day->updateCandle($candle->h, $candle->l, $candle->c);
+    }
+    */
+
+
+    if ($i >= 1)
+    {
+        CandleManager::getInstance()->addNewCandle($candle);
+    }
+
+    $prev_candle = $candle;
+}
+
+
+var_dump(count(CandleManager::getInstance()->candle_data_list[1]));
+
+// 계정 셋팅
+$account = Account::getInstance();
+$account->balance = 1;
+
+
+$candle = CandleManager::getInstance()->getFirstCandle(240)->getCandleNext();
+$prev_candle = $candle;
+
+
+$str = <<<HTML
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<table border="1">
+    <tr>
+        <td>주문시간</td>
+        <td>하이</td>
+        <td>로우</td>
+        <td>오픈</td>
+        <td>클로즈</td>
+        <td>변동가격</td>
+        <td>퍼센트</td>
+    </tr>
+HTML;
+
+$fp = fopen("vol_result.htm", "w");
+fwrite($fp, $str);
+var_dump($candle->getDateTime());
+for ($i=0; $i<500000000; $i++)
+{
+    if ($candle == null)
+    {
+        break;
+    }
+    \trading_engine\util\CoinPrice::getInstance()->updateBitPrice($candle->c);
+    //\trading_engine\managers\OrderManager::getInstance()->update($candle);
+
+    //\trading_engine\strategy\StrategyBB::getInstance()->BBS($candle->cp);
+    //\trading_engine\strategy\StrategyTest::getInstance()->BBS($candle);
+
+    $volPrice = $candle->getVolatilityValue(4);
+    $cPrice = $candle->c;
+    $per = $volPrice / $cPrice * 100;
+    $str = <<<HTML
+    <tr>
+        <td>{$candle->getDateTime()}</td>
+        <td>{$candle->h}</td>
+        <td>{$candle->l}</td>
+        <td>{$candle->o}</td>
+        <td>{$candle->c}</td>
+        <td>{$volPrice}</td>
+        <td>{$per}</td> 
+    </tr>
+HTML;
+    fwrite($fp, $str);
+
+
+
+    $prev_candle = $candle;
+    $candle = $candle->cn;
+}
+
+var_dump($prev_candle->getDateTime());
+
+// echo $money will output "123.1";
+//$len = fprintf($fp, '%01.2f', $money);/
+// will write "123.10" to currency.txt
+
+//ob_end_clean();
+
+TradeLogManager::getInstance()->showResultHtml();
+
+$result_time = time() - $time_start;
+echo "end. time : ".$result_time;
+
+var_dump($account->getUSDBalanceFloat());
+var_dump($account->getBitBalance());
+var_dump($prev_candle->getDateTime());
+var_dump(\trading_engine\managers\OrderManager::getInstance());
