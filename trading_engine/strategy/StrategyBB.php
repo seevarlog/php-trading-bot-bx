1652872b3181720d356bb0d7ff14a0d0bc8add18
seevarlog <sung135907@naver.com>
2021-02-23 21:44:29 +0900

    - 전략 수정

--- trading_engine/strategy/StrategyBB.php
+++ trading_engine/strategy/StrategyBB.php
@@ -56,10 +56,15 @@ class StrategyBB extends StrategyBase
         $log_min = "11111111";
         $sideCount = $candle_60min->getSidewaysCount($this->side_length);
         $vol = $candle_60min->getAvgRealVolatilityPercent(40);
-        if ($sideCount <= $this->side_count && $vol > 0.022)
+        if ($sideCount <= $this->side_count && $vol > 0.033)
+        {
+            $log_min = "555555555";
+            $candle = $candle_5min;
+        }
+        else if ($sideCount <= $this->side_count && $vol > 0.022)
         {
             $log_min = "333333333";
-            $candle = $candle_5min;
+            $candle = $candle_3min;
         }
 
         GlobalVar::getInstance()->candleTick = $candle->tick;
--- trading_engine/strategy/StrategyBBShort.php
+++ trading_engine/strategy/StrategyBBShort.php
@@ -63,10 +63,15 @@ class StrategyBBShort extends StrategyBase
         $log_min = "11111111";
         $sideCount = $candle_60min->getSidewaysCount($this->side_length);
         $vol = $candle_60min->getAvgRealVolatilityPercent(40);
-        if ($sideCount <= $this->side_count && $vol > 0.022)
+        if ($sideCount <= $this->side_count && $vol > 0.033)
+        {
+            $log_min = "555555555";
+            $candle = $candle_5min;
+        }
+        else if ($sideCount <= $this->side_count && $vol > 0.0222)
         {
             $log_min = "333333333";
-            $candle = $candle_5min;
+            $candle = $candle_3min;
         }
         GlobalVar::getInstance()->candleTick = $candle->tick;
         GlobalVar::getInstance()->CrossCount = $sideCount;
