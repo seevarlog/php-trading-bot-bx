b9d4005a50d316a70d9d443384d5e615ac58ec4c
seevarlog <sung135907@naver.com>
2021-03-03 00:23:00 +0900

    - 전략 수정

--- trading_engine/strategy/StrategyBBShort.php
+++ trading_engine/strategy/StrategyBBShort.php
@@ -63,7 +63,7 @@ class StrategyBBShort extends StrategyBase
         $log_min = "11111111";
         $sideCount = $candle_60min->getSidewaysCount($this->side_length);
         $vol = $candle_60min->getAvgRealVolatilityPercent(40);
-        if ($sideCount <= $this->side_count && $vol > 0.018)
+        if ($sideCount <= $this->side_count && $vol > 0.016)
         {
             $log_min = "555555555";
             $candle = $candle_5min;
