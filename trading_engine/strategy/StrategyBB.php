99f8a5e7a55eb1330da1a93d86c0835a687cac17
seongjeon <seongjeon@com2us.com>
2021-03-25 17:18:23 +0900

    - 전략 ㅜ섲ㅇ

--- trading_engine/strategy/StrategyBB.php
+++ trading_engine/strategy/StrategyBB.php
@@ -378,7 +378,7 @@ class StrategyBB extends StrategyBase
             }
             else
             {
-                $leverage_correct = $leverage - ($leverage - ($leverage_standard_stop_per / $leverage_stop_per * $leverage));
+                $leverage_correct = $leverage - ($leverage - ($leverage_standard_stop_per / $leverage_stop_per * $leverage)) / 1.3;
             }
         }
 
