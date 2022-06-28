a38d9469f5d26676fd176d1578b6a18fbd8e91fa
Jeon Seong <sung135790@naver.com>
2022-06-29 00:15:11 +0900

    - adx 200 수정

--- trading_engine/objects/Candle.php
+++ trading_engine/objects/Candle.php
@@ -1661,7 +1661,8 @@ class Candle
     {
         if ($left == -1)
         {
-            $left = 60;
+            $left = $length * 5;
+            $left = max($left, 300);
         }
 
         if (isset($this->d_p[$length]))
@@ -1724,7 +1725,8 @@ class Candle
     {
         if ($left == -1)
         {
-            $left = 60;
+            $left = $length * 5;
+            $left = max($left, 300);
         }
 
         if (isset($this->d_m[$length]))
@@ -1837,7 +1839,8 @@ class Candle
     {
         if ($left == -1)
         {
-            $left = 50;
+            $left = $length * 5;
+            $left = max($left, 300);
         }
 
         if (isset($this->adx[$length]))
